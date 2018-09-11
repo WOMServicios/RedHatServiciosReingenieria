2a6559447c918d3c3193a63d3e36bf6ad3ee5880
CarlosSanchez <hypetech.cl@gmail.com>
2018-09-11 16:38:32 -0300

    SOAProvisionVoLTEV1

---
+++ SRV40011-UnifiedSalesSupportService/src/main/java/cl/wom/exception/services/ServiceError.java
@@ -0,0 +1,49 @@
+package cl.wom.exception.services;
+
+public class ServiceError extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	 private int codigoError;
+
+	public ServiceError(String msg) {
+	
+	    super(msg);
+		this.codigoError=Integer.parseInt(msg);
+		getMessage();
+	}
+	
+	
+	 
+	
+	 @Override
+	    public String getMessage(){
+	         
+	        String mensaje="";
+	         
+	        switch(codigoError){
+	            case 400:
+	                mensaje="Error, 400 prueba";
+	                break;
+	            case 500:
+	                mensaje="Error, 500 prueba";
+	                break;
+	            case 503:
+	                mensaje="Error, prueba";
+	                break;
+	                
+	             default:
+	            	   mensaje="Error, sin codigo de error reconocido";
+	            	 break;
+	        }
+	         
+	        return mensaje;
+	         
+	    }
+	 
+	 public int getCodigoError() {
+		 return codigoError;
+	 }
+	 
+	
+}
