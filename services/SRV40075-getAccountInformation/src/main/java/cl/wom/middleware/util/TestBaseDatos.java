79710a92915b84c171187cd5fdc57212952b68a0
Cristian Quezada <cquezada@cstit.cl>
2018-10-16 14:45:52 -0300

    actualizacion account

--- services/SRV40075-getAccountInformation/src/main/java/cl/wom/middleware/util/TestBaseDatos.java
+++ services/SRV40075-getAccountInformation/src/main/java/cl/wom/middleware/util/TestBaseDatos.java
@@ -20,19 +20,18 @@ import cl.wom.middleware.vo.Subscribers;
 public class TestBaseDatos {
 
 	public static void main(String[] args) {
-		new TestBaseDatos().getProductCatalog("010567335","");
+		new TestBaseDatos().getProductCatalog("","602372");
+//		new TestBaseDatos().getProductCatalog("010567335","");
 	}
 	
 	
 	public void getProductCatalog(String rut, String accountdID)  {
 		
-//		System.setProperty("database.bscs.host", "10.120.241.44");
+		System.setProperty("database.bscs.host", "10.120.241.44");
 		System.setProperty("database.bscs.port", "1540");
 		System.setProperty("database.bscs.databasename", "BSCSUAT");
 		System.setProperty("database.bscs.username", "VMD");
 		System.setProperty("database.bscs.password", "VMD");
-		
-		System.out.println("conn: "+System.getenv("datoCQT"));
 		
 		Connection conn = null;
 		Statement stmt;
@@ -41,10 +40,10 @@ public class TestBaseDatos {
 			stmt = conn.createStatement();
 			
 			
+			//1.1
+			String whereCondition = rut.equals("")?"a.customer_id = "+accountdID+"":"a.cscompregno = '"+rut+"'";
 			
-			
-
-			String queryAccounts = "SELECT a.cscompregno      as rut,a.CUSTOMER_ID      as accountId,a.CUSTOMER_ID_HIGH as accountIdHigh,a.cslevel          as csLevel,a.CUSTCODE         as custCode,a.CSTYPE           as accountType,a.CSACTIVATED      as accountActivate,a.CSDEACTIVATED    as accountDeactivate,a.CUSTOMER_ID_EXT  as externalAccountId,a.CSST             as state,a.DOCTYPE_ID       as docTypeId,b.DOCTYPE_DESC     as docTypeDesc,b.DOCTYPE_OUTPUT_CODE as docTypeOutputCode,a.CUSTOMER_ID      as accountId_biilcycle,d.BILLCYCLE        as billCycle_billcycle,d.description      as billCycleDes_billcycle,d.interval_type    as intervalType,d.last_run_date    as lastRunDate,d.bch_run_date     as bchRunDate FROM sysadm.customer_all           a, sysadm.DOCUMENT_TYPE_SII_CODE b, SYSADM.BILLCYCLE_ACTUAL_VIEW  c, SYSADM.BILLCYCLES  d WHERE a.cscompregno = '010567335' and a.DOCTYPE_ID  = b.DOCTYPE_ID and a.CUSTOMER_ID = c.CUSTOMER_ID and c.billcycle   = d.billcycle";
+			String queryAccounts = "SELECT a.cscompregno      as rut,a.CUSTOMER_ID      as accountId,a.CUSTOMER_ID_HIGH as accountIdHigh,a.cslevel          as csLevel,a.CUSTCODE         as custCode,a.CSTYPE           as accountType,a.CSACTIVATED      as accountActivate,a.CSDEACTIVATED    as accountDeactivate,a.CUSTOMER_ID_EXT  as externalAccountId,a.CSST             as state,a.DOCTYPE_ID       as docTypeId,b.DOCTYPE_DESC     as docTypeDesc,b.DOCTYPE_OUTPUT_CODE as docTypeOutputCode,a.CUSTOMER_ID      as accountId_biilcycle,d.BILLCYCLE        as billCycle_billcycle,d.description      as billCycleDes_billcycle,d.interval_type    as intervalType,d.last_run_date    as lastRunDate,d.bch_run_date     as bchRunDate FROM sysadm.customer_all           a, sysadm.DOCUMENT_TYPE_SII_CODE b, SYSADM.BILLCYCLE_ACTUAL_VIEW  c, SYSADM.BILLCYCLES  d WHERE "+ whereCondition+" and a.DOCTYPE_ID  = b.DOCTYPE_ID and a.CUSTOMER_ID = c.CUSTOMER_ID and c.billcycle   = d.billcycle";
 			ResultSet rsAccounts = conn.createStatement().executeQuery(queryAccounts);
 			
 			
@@ -52,35 +51,50 @@ public class TestBaseDatos {
 			accountInformation.setRut(rut);
 			List<Account> listAccount = new ArrayList<Account>();
 			List<BillCycle> listaBillCycle = new ArrayList<BillCycle>();
-			List<Subscribers> listasubscribers = new ArrayList<Subscribers>();
-			List<SubscriberResources> listaSubscriberResources = new ArrayList<SubscriberResources>();
+			
+			
 			while (rsAccounts.next()) {
 				
 				Account account = new Account();
 				
-				Subscribers subscribers = new Subscribers();
-					subscribers.setRut(rsAccounts.getString("rut"));
-					subscribers.setAccountId(rsAccounts.getString("accountId"));
+				
+				//Incorporación de subscriptores al objeto account
+				List<Subscribers> listasubscribers = new ArrayList<Subscribers>();
+				//2.1
+				String querySubscribers = "SELECT a.cscompregno      as rut,b.customer_id      as accountId,b.co_id            as subscriberId,b.type             as subscriberType, b.co_code          as subscriberIdContract, b.CO_SIGNED        as subscriberActivate,b.CO_EXPIR_DATE    as subscriberExpired,b.CH_STATUS        as state FROM sysadm.customer_all           a, SYSADM.contract_all           b, SYSADM.CONTRACT_HISTORY       c WHERE  "+whereCondition+" and a.customer_id = b.customer_id and b.co_id       = c.co_id and c.ch_status   = 'a'";
+				ResultSet rsSubscribers = conn.createStatement().executeQuery(querySubscribers);
+				while(rsSubscribers.next()) {
 					
-					SubscriberResources subscriberResources = new SubscriberResources();
-//					subscriberResources.setResourceId(rsAccounts.getString("resourceId"));
-//					subscriberResources.setResourceDeactivate(rsAccounts.getString("resourceDeactivate"));
-//					subscriberResources.setResource(rsAccounts.getString("resource"));
-//					subscriberResources.setSubscriberId(rsAccounts.getString("subscriberId"));
-//					subscriberResources.setResourceDescription(rsAccounts.getString("resourceDescription"));
-//					subscriberResources.setResourceState(rsAccounts.getString("resourceState"));
-//					subscriberResources.setResourceActivate(rsAccounts.getString("resourceActivate"));
-//					subscriberResources.setResourceType(rsAccounts.getString("resourceType"));
-					listaSubscriberResources.add(subscriberResources);
-					
-//					subscribers.setSubscriberIdContract(rsAccounts.getString("subscriberIdContract"));
-//					subscribers.setSubscriberType(rsAccounts.getString("subscriberType"));
-//					subscribers.setSubscriberActivate(rsAccounts.getString("subscriberActivate"));
-//					subscribers.setSubscriberId(rsAccounts.getString("subscriberId"));
-					subscribers.setState(rsAccounts.getString("state"));
-//					subscribers.setSubscriberExpired(rsAccounts.getString("subscriberExpired"));
-					System.out.println("subscriberRUT:" + rsAccounts.getString("rut"));
+					Subscribers subscribers = new Subscribers();
+					subscribers.setRut(rsSubscribers.getString("rut"));
+					subscribers.setAccountId(rsSubscribers.getString("accountId"));
+					subscribers.setSubscriberIdContract(rsSubscribers.getString("subscriberIdContract"));
+					subscribers.setSubscriberType(rsSubscribers.getString("subscriberType"));
+					subscribers.setSubscriberActivate(rsSubscribers.getString("subscriberActivate"));
+					subscribers.setSubscriberId(rsSubscribers.getString("subscriberId"));
+					subscribers.setState(rsSubscribers.getString("state"));
+					subscribers.setSubscriberExpired(rsSubscribers.getString("subscriberExpired"));
+			
+//					List<SubscriberResources> listaSubscriberResources = new ArrayList<SubscriberResources>();
+//					while(true) {
+//						SubscriberResources subscriberResources = new SubscriberResources();
+//						subscriberResources.setResourceId(rsAccounts.getString("resourceId"));
+//						subscriberResources.setResourceDeactivate(rsAccounts.getString("resourceDeactivate"));
+//						subscriberResources.setResource(rsAccounts.getString("resource"));
+//						subscriberResources.setSubscriberId(rsAccounts.getString("subscriberId"));
+//						subscriberResources.setResourceDescription(rsAccounts.getString("resourceDescription"));
+//						subscriberResources.setResourceState(rsAccounts.getString("resourceState"));
+//						subscriberResources.setResourceActivate(rsAccounts.getString("resourceActivate"));
+//						subscriberResources.setResourceType(rsAccounts.getString("resourceType"));
+//						listaSubscriberResources.add(subscriberResources);
+//					}					
 					listasubscribers.add(subscribers);
+				}
+				account.setSubscribers(listasubscribers); 
+
+				
+				
+				
 					
 				//account.setSubscribers(subscribers);
 				account.setAccountType(rsAccounts.getString("accountType"));
@@ -97,34 +111,17 @@ public class TestBaseDatos {
 				account.setAccountIdHigh(rsAccounts.getString("accountIdHigh"));
 				account.setState(rsAccounts.getString("state"));
 				
+				//Incorporación de BillCycle
 				BillCycle billCycle = new BillCycle();
-					billCycle.setBchRunDate(rsAccounts.getString("bchRunDate"));
-					billCycle.setAccountId(rsAccounts.getString("accountId"));
-					billCycle.setIntervalType(rsAccounts.getString("intervalType"));
-					billCycle.setBillCycleDes(rsAccounts.getString("billCycleDes_billcycle"));
-					billCycle.setLastRunDate(rsAccounts.getString("lastRunDate"));
-					billCycle.setBillCycle(rsAccounts.getString("billCycle_billcycle"));
-					
-					listaBillCycle.add(billCycle);
-					
+				billCycle.setBchRunDate(rsAccounts.getString("bchRunDate"));
+				billCycle.setAccountId(rsAccounts.getString("accountId"));
+				billCycle.setIntervalType(rsAccounts.getString("intervalType"));
+				billCycle.setBillCycleDes(rsAccounts.getString("billCycleDes_billcycle"));
+				billCycle.setLastRunDate(rsAccounts.getString("lastRunDate"));
+				billCycle.setBillCycle(rsAccounts.getString("billCycle_billcycle"));
+				listaBillCycle.add(billCycle);
 				account.setBillCycle(billCycle);
 				
-//				ProductOffering productOffering = new ProductOffering();
-//				
-//				productOffering.setLastUpdate(rsGetProductOffering.getString("lastUpdate"));
-				
-				
-//				List<BundleProductOffering> listBundleProductOffering = new ArrayList<BundleProductOffering>();
-//				String queryGetBundleOfferingA = "select b.ID_PROD as id, a.tmcode as offerId, a.SHDES as shDes, b.NAME_PROD as name, b.DESC_PROD as description, b.LEVEL_PCRF_PROD as priority, b.status  as status, '1' as minimumRequired, '1' as maximumAllowed, 'false' as isOfferProduct, 'true' as isOptionProduct, decode(b.promo,'S','True','N','False') as isPromotionProduct, b.file1 as occ, b.sku as sku, b.CHANNEL_ACT as name_channel, b.CHANNEL_ACT as legacySystem_channel, decode(b.recurrence,'N',b.tariff_prod) as amount_oneTime, decode(b.recurrence,'N','CLP') as currency_oneTime, decode(b.recurrence,'N',b.VIGENCIA) as duration_oneTime, decode(b.recurrence,'N','Minutos') as unitOfMesaure_oneTime, decode(b.recurrence,'N','Bundle') as type_oneTime, decode(b.recurrence,'S',b.tariff_prod) as amount_recurringCharge, decode(b.recurrence,'S','CLP') as currency_recurringCharge, decode(b.recurrence,'S',b.VIGENCIA) as duration_recurringCharge, decode(b.recurrence,'S','Minutos') as unitOfMesaure_recurringCharge, decode(b.recurrence,'S','Bundle') as type_recurringCharge from sysadm.rateplan a, PROVI_BOLS.NEXTEL_CATALOGO_PRODUCTOS b "
-//						+ "where a.tmcode="+OfferID+" and ROWNUM <= 10 ---variable   and REGEXP_LIKE(to_char(a.tmcode), '^(|'||REPLACE(REPLACE(UPPER(FAMILY_PLAN),'',UPPER(to_char(a.tmcode))),';','|')||')$')";
-//				ResultSet rsGetBundleOfferingA = conn.createStatement().executeQuery(queryGetBundleOfferingA);
-//				while (rsGetBundleOfferingA.next()) {
-//					BundleProductOffering bundleProductOffering = new BundleProductOffering();
-//					bundleProductOffering.setName(rsGetBundleOfferingA.getString("name"));
-//					
-//					listBundleProductOffering.add(bundleProductOffering);
-//				}
-//				productOffering.setBundleProductOffering(listBundleProductOffering);
 				
 				
 				
