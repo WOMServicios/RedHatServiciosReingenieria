0794782e83427f8b30496a2379022d75138803cf
cesar <cecv12@gmail.com>
2018-10-16 11:53:25 -0300

    actualiza

--- services/SRV40075-getAccountInformation/src/main/java/cl/wom/database/Ejecutasql.java
+++ services/SRV40075-getAccountInformation/src/main/java/cl/wom/database/Ejecutasql.java
@@ -8,42 +8,26 @@ import java.sql.Statement;
 import cl.wom.database.ConnectionFactory.DataBaseSchema;
 
 public class Ejecutasql {
-	
-	public static void main() throws ClassNotFoundException, SQLException {
-		
+
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
 		Connection conn = ConnectionFactory.getConnection(DataBaseSchema.BSCSUAT);
-		
+
 		Statement stmt = null;
-		String query = "select\n" + 
-				"a.cscompregno      as rut,\n" + 
-				"a.CUSTOMER_ID      as accountId,\n" + 
-				"a.CUSTOMER_ID_HIGH as accountIdHigh,\n" + 
-				"a.cslevel          as csLevel,\n" + 
-				"a.CUSTCODE         as custCode,\n" + 
-				"a.CSTYPE           as accountType,\n" + 
-				"a.CSACTIVATED      as accountActivate,\n" + 
-				"a.CSDEACTIVATED    as accountDeactivate,\n" + 
-				"a.CUSTOMER_ID_EXT  as externalAccountId,\n" + 
-				"a.CSST             as state,\n" + 
-				"a.DOCTYPE_ID       as docTypeId,\n" + 
-				"b.DOCTYPE_DESC     as docTypeDesc,\n" + 
-				"b.DOCTYPE_OUTPUT_CODE as docTypeOutputCode,\n" + 
-				"a.CUSTOMER_ID      as accountId_biilcycle,\n" + 
-				"d.BILLCYCLE        as billCycle_billcycle,\n" + 
-				"d.description      as billCycleDes_billcycle,\n" + 
-				"d.interval_type    as intervalType,\n" + 
-				"d.last_run_date    as lastRunDate,\n" + 
-				"d.bch_run_date     as bchRunDate\n" + 
-				"from\n" + 
-				"    sysadm.customer_all           a,\n" + 
-				"    sysadm.DOCUMENT_TYPE_SII_CODE b,\n" + 
-				"    SYSADM.BILLCYCLE_ACTUAL_VIEW  c,\n" + 
-				"    SYSADM.BILLCYCLES             d\n" + 
-				"where\n" + 
-				"    a.cscompregno = '010567335'--variable\n" + 
-				"and a.DOCTYPE_ID  = b.DOCTYPE_ID\n" + 
-				"and a.CUSTOMER_ID = c.CUSTOMER_ID\n" + 
-				"and c.billcycle   = d.billcycle;";
+		String query = "select\n" + "a.cscompregno      as rut,\n" + "a.CUSTOMER_ID      as accountId,\n"
+				+ "a.CUSTOMER_ID_HIGH as accountIdHigh,\n" + "a.cslevel          as csLevel,\n"
+				+ "a.CUSTCODE         as custCode,\n" + "a.CSTYPE           as accountType,\n"
+				+ "a.CSACTIVATED      as accountActivate,\n" + "a.CSDEACTIVATED    as accountDeactivate,\n"
+				+ "a.CUSTOMER_ID_EXT  as externalAccountId,\n" + "a.CSST             as state,\n"
+				+ "a.DOCTYPE_ID       as docTypeId,\n" + "b.DOCTYPE_DESC     as docTypeDesc,\n"
+				+ "b.DOCTYPE_OUTPUT_CODE as docTypeOutputCode,\n" + "a.CUSTOMER_ID      as accountId_biilcycle,\n"
+				+ "d.BILLCYCLE        as billCycle_billcycle,\n" + "d.description      as billCycleDes_billcycle,\n"
+				+ "d.interval_type    as intervalType,\n" + "d.last_run_date    as lastRunDate,\n"
+				+ "d.bch_run_date     as bchRunDate\n" + "from\n" + "    sysadm.customer_all           a,\n"
+				+ "    sysadm.DOCUMENT_TYPE_SII_CODE b,\n" + "    SYSADM.BILLCYCLE_ACTUAL_VIEW  c,\n"
+				+ "    SYSADM.BILLCYCLES             d\n" + "where\n" + "    a.cscompregno = '010567335'--variable\n"
+				+ "and a.DOCTYPE_ID  = b.DOCTYPE_ID\n" + "and a.CUSTOMER_ID = c.CUSTOMER_ID\n"
+				+ "and c.billcycle   = d.billcycle;";
 		try {
 			stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
@@ -51,10 +35,12 @@ public class Ejecutasql {
 				String rut = rs.getString("RUT");
 				System.out.println(rut);
 			}
-		} cacth (SQLException e){
-			
+		} catch (SQLException e) {
+			System.out.println("error");
+		} finally {
+			if (stmt != null) {
+				stmt.close();
+			}
 		}
-		
 	}
-
 }
