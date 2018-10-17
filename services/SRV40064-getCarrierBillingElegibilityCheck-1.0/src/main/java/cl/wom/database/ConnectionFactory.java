9df1bd8035a99ae30ca93cc9325795c42978d093
Alejandro <ale.fvaras@gmail.com>
2018-10-17 12:45:06 -0300

    subir para pruebas miercoles

--- services/SRV40064-getCarrierBillingElegibilityCheck-1.0/src/main/java/cl/wom/database/ConnectionFactory.java
+++ services/SRV40064-getCarrierBillingElegibilityCheck-1.0/src/main/java/cl/wom/database/ConnectionFactory.java
@@ -4,69 +4,42 @@ import java.sql.Connection;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.ResourceBundle;
+import java.util.Properties;
 
-import org.apache.camel.language.Bean;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
+import cl.wom.util.Util;
+
 
 
 public class ConnectionFactory {
 
-	
-	
-	
 	static Connection con = null;
-	static public enum DataBaseSchema {
-	    BSCS,
-	    WAPPL;
-	}
-	
 
-//	public static Connection conectar(String password,String user,String host,String port,String databaseName) {
-//		  try {
-//			Class.forName("oracle.jdbc.driver.OracleDriver");
-//		} catch (ClassNotFoundException e1) {
-//			// TODO Auto-generated catch block
-//			e1.printStackTrace();
-//		}
-//		
-//
-//		try {
-//			
-//			con = DriverManager.getConnection("jdbc:oracle:thin:@" + host + ":" + port + ":" + databaseName, user,
-//					password);
-////			con=DriverManager.getConnection("jdbc:oracle:thin:@10.120.148.136:1521:WAPPLDESA","CARRIERBILLING","carrierdes09");
-//			if (con != null) {
-//				System.out.println("Conectado");
-//			}
-//		} catch (SQLException e) {
-//			System.out.println("No se pudo conectar a la base de datos");
-//			e.printStackTrace();
-//		}
-//		return con;
+	static public enum DataBaseSchema {
+		BSCS, WAPPL;
+	}
+
+	static Properties prop = Util.getProperties("APP_ENV");
 
 	public static Connection getConnection(DataBaseSchema shema) throws ClassNotFoundException, SQLException {
-//		ResourceBundle rb = ResourceBundle.getBundle("application.properties");
+
 		Class.forName("oracle.jdbc.driver.OracleDriver");
-
-		
 		switch (shema) {
-			case BSCS:
-				return DriverManager.getConnection("jdbc:oracle:thin:@" + "10.120.241.44" + ":" + "1550" + ":" + "BSCSDESA", "OPSH_BSCSREP_SYSADM_CBI","knh#5tgl20k0lpm.l");
-			case WAPPL:
-				return DriverManager.getConnection("jdbc:oracle:thin:@" + "10.120.148.136" + ":" + "1521" + ":" + "WAPPLDESA", "CARRIERBILLING","carrierdes09");
+		case BSCS:
+			return DriverManager.getConnection(
+					"jdbc:oracle:thin:@" + prop.getProperty("database.replica.host") + ":"
+							+ prop.getProperty("database.replica.port") + ":" + prop.getProperty("database.replica.name"),
+					prop.getProperty("database.replica.username"), prop.getProperty("database.replica.password"));
+		case WAPPL:
 
-//			return DriverManager.getConnection("jdbc:oracle:thin:@" + rb.getString("${database.wappl.host}") + ":" + rb.getString("${database.wappl.port}") + ":" + rb.getString("${database.wappl.name}"), rb.getString("${database.wappl.username}"),	rb.getString("${database.wappl.password}"));				
+			return DriverManager.getConnection(
+					"jdbc:oracle:thin:@" + prop.getProperty("database.wappl.host") + ":"
+							+ prop.getProperty("database.wappl.port") + ":" + prop.getProperty("database.wappl.name"),
+					prop.getProperty("database.wappl.username"), prop.getProperty("database.wappl.password"));
+
 		default:
 			return null;
 		}
-		
-	}
-	
 
-	
+	}
+
 }
