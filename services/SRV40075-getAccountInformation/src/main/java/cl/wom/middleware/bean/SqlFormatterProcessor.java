5b857b8aec9d2a3443edbb1a7ba2d73fd403746e
Cristian Quezada <cquezada@cstit.cl>
2018-10-17 13:45:52 -0300

    commit

---
+++ services/SRV40075-getAccountInformation/src/main/java/cl/wom/middleware/bean/SqlFormatterProcessor.java
@@ -0,0 +1,44 @@
+package cl.wom.middleware.bean;
+
+import org.apache.camel.Exchange;
+import org.json.JSONObject;
+
+import cl.wom.middleware.dao.AccountManagerDAO;
+import cl.wom.middleware.vo.AccountInformation;
+
+public class SqlFormatterProcessor {
+	
+	public String sqlParserAccountInformation(Exchange ex) {
+		
+		String rut   = (String)ex.getIn().getHeader("rut");
+		String accountId  = (String)ex.getIn().getHeader("accountId");
+		
+		System.out.println("rut: "+rut);
+		System.out.println("accountId: "+accountId);
+ 
+		AccountManagerDAO accountManagerDAO = new AccountManagerDAO();
+		AccountInformation accountInformation = accountManagerDAO.getAccountInformation(rut, accountId);
+		
+		if (accountInformation != null) {
+			JSONObject jsonObj = new JSONObject(accountInformation);
+			return jsonObj.toString();
+		}else {
+			return null;
+		}
+	}
+	
+	
+	public String sqlGetRutAccountManager(Exchange ex) {
+		
+//		String rut   = (String)ex.getIn().getHeader("rut");
+//		String accountId  = (String)ex.getIn().getHeader("accountId");
+		
+ 
+		AccountManagerDAO accountManagerDAO = new AccountManagerDAO();
+		String rut = accountManagerDAO.getResourceInformation("", "");
+		
+		return rut;
+	}
+	
+	
+}
