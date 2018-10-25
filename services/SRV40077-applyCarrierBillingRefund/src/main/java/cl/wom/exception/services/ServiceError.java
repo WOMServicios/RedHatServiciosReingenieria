3a77daecd6928d7dfc6d76bd7dade36e4e2f4c70
cesar <cecv12@gmail.com>
2018-10-25 12:37:14 -0300

    se crear servicios SRV40077-carrierbillingrefund

---
+++ services/SRV40077-applyCarrierBillingRefund/src/main/java/cl/wom/exception/services/ServiceError.java
@@ -0,0 +1,63 @@
+package cl.wom.exception.services;
+
+/**
+ * Esta clase maneja los errores y devuelve codigos customizados
+ * 
+ * @author Alejandro Fernandez
+ * @date 08/2018
+ */
+
+public class ServiceError extends Exception{
+
+   private static final long serialVersionUID = 1L;
+   private int codigoError;
+
+   /**
+    * Constructor de la clase que invoca al metodo getMessage()
+    * @param msg Mensaje de error que es parseado para retornar el error customizado
+    */
+   public ServiceError(String msg) {
+      super(msg);
+      this.codigoError=Integer.parseInt(msg);
+      getMessage();
+   }
+   
+   /**
+    * Metodo que retorna el mensaje de error customizado dependiendo del tipo
+    * @return un mensaje de error
+    */
+   @Override
+   public String getMessage(){
+	   String mensaje="";
+	   
+	   switch(codigoError){
+	   case 400:
+		   mensaje="bad request";
+		   break;
+	   case 453:
+		   mensaje="No Data Found";
+		   break;
+	   case 454:
+		   mensaje="Unprocessable Entity";
+		   break;
+	   case 452:
+		   mensaje="Excceds the maximun length";
+		   break;
+	   case 455:
+		   mensaje="DataBase Error SQL Exception";
+		   break;		   
+	   }
+	   		   
+	   
+	   
+	   return mensaje;
+   }
+   
+   /**
+    * Recibe el codigo de error y lo retorna
+    * @return un entero que especifica el codigo de error
+    */
+   public int getCodigoError() {
+		 return codigoError;
+   }
+}
