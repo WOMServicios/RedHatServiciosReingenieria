96c8e2892aad211b69415e052338076d539e7152
cesar <cecv12@gmail.com>
2018-10-29 14:12:12 -0300

    act

--- services/SRV40077-applyCarrierBillingRefund-1.0/src/main/java/cl/wom/middleware/dao/CarrierRefundDAO.java
+++ services/SRV40077-applyCarrierBillingRefund-1.0/src/main/java/cl/wom/middleware/dao/CarrierRefundDAO.java
@@ -37,49 +37,54 @@ public class CarrierRefundDAO {
 			String queryRefund = "SELECT COUNT(USER_ID) AS CONTADOR FROM CARRIERBILLING.RECEP_PAGOS_CARRIER_BILING_TO WHERE USER_ID = TRIM('"
 					+ userId + "') AND PAYMENT_PROVIDER_TRANSACTION = TRIM('" + payment
 					+ "') AND RESPONSE_PAY ='OK' AND ACTION ='REFUND'";
-			 System.out.println("queryRefund: "+queryRefund);
-
+			System.out.println("queryRefund: " + queryRefund + "\n");
 			ResultSet rsContRefund = stmtWAPP.executeQuery(queryRefund);
+			rsContRefund.next();
 
-			if (rsContRefund.next()) {
-				System.out.println("Contador: " + rsContRefund.getInt("CONTADOR"));
+			if (rsContRefund.getInt("CONTADOR") > 0) {
+				// TODO enviar al camel para velocity
+				System.out.println("ALREADY_REFUNDED");
+			} else {
+				String queryCharge = "SELECT COUNT(USER_ID) AS CONTADOR FROM CARRIERBILLING.RECEP_PAGOS_CARRIER_BILING_TO WHERE USER_ID = TRIM('"
+						+ userId + "') AND PAYMENT_PROVIDER_TRANSACTION = TRIM('" + payment
+						+ "') AND RESPONSE_PAY ='OK' AND ACTION ='CHARGE'";
+				System.out.println("queryCharge: " + queryCharge + "\n");
+				ResultSet rsContCharge = stmtWAPP.executeQuery(queryCharge);
+				rsContCharge.next();
 
-				if (rsContRefund.getInt("CONTADOR") > 0) {
-
+				if (rsContCharge.getInt("CONTADOR") == 1) {
 					// TODO enviar al camel para velocity
-					System.out.println("ALREADY_REFUNDED");
+					System.out.println("USER_NOT_ENABLED");
 				} else {
-					String queryCharge = "SELECT COUNT(USER_ID) AS CONTADOR FROM CARRIERBILLING.RECEP_PAGOS_CARRIER_BILING_TO WHERE USER_ID = TRIM('"
-							+ userId + "') AND PAYMENT_PROVIDER_TRANSACTION = TRIM('" + payment
-							+ "') AND RESPONSE_PAY ='OK' AND ACTION ='CHARGE'";
-					System.out.println("queryCharge: "+queryCharge);
+					String queryDatos = "select a.dn_num as msisdn, c.customer_id as customer_id, c.co_id as co_id, c.tmcode as rate_plan, decode(d.ATS_PREPAID_IND,'M','Control','N','Postpaid','P','Prepaid') as tipo_Contrato, c.CH_STATUS as estado, c.CO_ACTIVATED as fecha_activacion, b.cs_deactiv_date as fecha_desactivacion from sysadm.directory_number a, sysadm.contr_services_cap b, sysadm.contract_all c, sysadm.rateplan d where c.co_id = '"
+							+ userId
+							+ "'and a.dn_id = b.dn_id and b.sncode = 3 and b.co_id = c.co_id AND b.cs_deactiv_date IS NULL and c.tmcode = d.tmcode";
+					System.out.println("queryDatos: " + queryDatos + "\n");
 
-					ResultSet rsContCharge = stmtWAPP.executeQuery(queryCharge);
+					ResultSet rsDatos = stmtBCSC.executeQuery(queryDatos);
 
-					if (rsContCharge.next()) {
-						System.out.println("Contador: " + rsContCharge.getInt("CONTADOR"));
-						if (rsContCharge.getInt("CONTADOR") == 1) {
+					if (rsDatos.next()) {
 
+						System.out.println("segunda parte \n");
+
+						if (rsDatos.getInt("CO_ID") == 0) {
 							// TODO enviar al camel para velocity
-							System.out.println("USER_NOT_ENABLE");
+							System.out.println("USER_NOT_FUND");
+						} else if (rsDatos.getString("TIPO_CONTRATO") != "Postpaid") {
+							// TODO enviar al camel para velocity
+							System.out.println("USER_NOT_ENABLE1");
+						} else if (rsDatos.getString("estado") == "s") {
+							// TODO enviar al camel para velocity
+							System.out.println("USER_SUSPENDED");
+						} else if (rsDatos.getString("estado") == "d" || rsDatos.getString("estado") == "o") {
+							// TODO enviar al camel para velocity
+							System.out.println("USER_NOT_ENABLE2");
+						} else {
+							System.out.println("CONSUMO DEL SERVICIO");
 						}
 
 					}
 
-				}
-
-			} else {
-				String queryDatos = "select a.dn_num as msisdn, c.customer_id as customer_id, c.co_id as co_id, c.tmcode as rate_plan, decode(d.ATS_PREPAID_IND,'M','Control','N','Postpaid','P','Prepaid') as tipo_Contrato, c.CH_STATUS as estado, c.CO_ACTIVATED as fecha_activacion, b.cs_deactiv_date as fecha_desactivacion from sysadm.directory_number a, sysadm.contr_services_cap b, sysadm.contract_all c, sysadm.rateplan d where c.co_id = '"
-						+ userId
-						+ "'and a.dn_id = b.dn_id and b.sncode = 3 and b.co_id = c.co_id AND b.cs_deactiv_date IS NULL and c.tmcode = d.tmcode";
-				System.out.println("queryDatos: " + queryDatos);
-				
-				ResultSet rsDatos = stmtBCSC.executeQuery(queryDatos);
-				
-				if(rsDatos.next()) {
-					
-					System.out.println("Query DAtos");
-					
 				}
 			}
 
