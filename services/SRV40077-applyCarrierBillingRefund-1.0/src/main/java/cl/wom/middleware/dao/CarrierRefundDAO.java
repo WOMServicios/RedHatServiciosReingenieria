d33bda531954b35d4c1bcc2199d6fd83ce08a579
cesar <cecv12@gmail.com>
2018-10-30 18:44:47 -0300

    se agregaron validaciones

--- services/SRV40077-applyCarrierBillingRefund-1.0/src/main/java/cl/wom/middleware/dao/CarrierRefundDAO.java
+++ services/SRV40077-applyCarrierBillingRefund-1.0/src/main/java/cl/wom/middleware/dao/CarrierRefundDAO.java
@@ -17,15 +17,12 @@ public class CarrierRefundDAO {
 	Statement stmtWAPP;
 	Statement stmtBCSC;
 
-	// TODO aplicar log y no system out
 	public  Refund getFacturacionRefund(String userId, String payment) throws ServiceError {
 
 		userId = userId == null ? "" : userId;
-		payment = payment == null ? "" : payment;
-
+		payment = payment == null ? "" : payment;	
+		
 		Refund refund = null;
-
-
 
 		try {
 
@@ -38,18 +35,15 @@ public class CarrierRefundDAO {
 			String queryRefund = "SELECT COUNT(USER_ID) AS CONTADOR FROM CARRIERBILLING.RECEP_PAGOS_CARRIER_BILING_TO WHERE USER_ID = TRIM('"
 					+ userId + "') AND PAYMENT_PROVIDER_TRANSACTION = TRIM('" + payment
 					+ "') AND RESPONSE_PAY ='OK' AND ACTION ='REFUND'";
-			//System.out.println("queryRefund: " + queryRefund + "\n");
 			ResultSet rsContRefund = stmtWAPP.executeQuery(queryRefund);
 			rsContRefund.next();
 
 			if (rsContRefund.getInt("CONTADOR") > 0) {
-				// TODO enviar al camel para velocity
 				throw new ServiceError("ALREADY_REFUNDED");
 			} else {
 				String queryCharge = "SELECT COUNT(USER_ID) AS CONTADOR FROM CARRIERBILLING.RECEP_PAGOS_CARRIER_BILING_TO WHERE USER_ID = TRIM('"
 						+ userId + "') AND PAYMENT_PROVIDER_TRANSACTION = TRIM('" + payment
 						+ "') AND RESPONSE_PAY ='OK' AND ACTION ='CHARGE'";
-				//System.out.println("queryCharge: " + queryCharge + "\n");
 				ResultSet rsContCharge = stmtWAPP.executeQuery(queryCharge);
 				rsContCharge.next();
 
@@ -59,7 +53,6 @@ public class CarrierRefundDAO {
 					String queryDatos = "select a.dn_num as msisdn, d.shdes as shdes_plan, c.customer_id as customer_id, c.co_id as co_id, c.tmcode as rate_plan, decode(d.ATS_PREPAID_IND,'M','Control','N','Postpaid','P','Prepaid') as tipo_Contrato, c.CH_STATUS as estado, c.CO_ACTIVATED as fecha_activacion, b.cs_deactiv_date as fecha_desactivacion from sysadm.directory_number a, sysadm.contr_services_cap b, sysadm.contract_all c, sysadm.rateplan d where c.co_id = '"
 							+ userId
 							+ "'and a.dn_id = b.dn_id and b.sncode = 3 and b.co_id = c.co_id AND b.cs_deactiv_date IS NULL and c.tmcode = d.tmcode";
-					//System.out.println("queryDatos: " + queryDatos + "\n");
 
 					ResultSet rsDatos = stmtBCSC.executeQuery(queryDatos);
 
@@ -115,7 +108,6 @@ public class CarrierRefundDAO {
 			
 		
 		String queryInsert = "INSERT INTO CARRIERBILLING.RECEP_PAGOS_CARRIER_BILING_TO ( REQUEST_ID , BANGO_TRANSACTION_ID , MERCHAN_TRANSACTION_ID , PAYMENT_PROVIDER_TRANSACTION , USER_ID , AMOUNT , CURRENCY , RESPONSE_PAY , DESCRIPTION_RESPONSE_PAY , DATE_PAY , OCCID, ACTION) VALUES('"+requestId+"','"+bangoTransactionId+"','"+merchantTransactionId+"','"+paymentProviderTransactionId+"','"+userId+"',"+amount+",'"+currency+"','"+responseCode+"','"+responseMessage+"',SYSDATE,'"+occId+"','REFUND')";
-			//System.out.println("query insert: " + queryInsert);
 		
 		ResultSet rsInsert = stmtWAPP.executeQuery(queryInsert);
 		
@@ -137,8 +129,4 @@ public class CarrierRefundDAO {
 		}
 		return null;
 	}
-	
-	
-	
-
 }
